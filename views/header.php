c3a7c083a087778ff8cf09ba1d558d7bb5122b7e
Алексей <59308915+AlekseyDolgov@users.noreply.github.com>
2022-07-24 13:04:25 +0300

    Update header.php

--- views/header.php
+++ views/header.php
@@ -15,37 +15,5 @@
         <p>-Джордж Оруэлл</p>
     </small>
 </header>
-<!--
-<aside>
-    <figure>
-        <img src="style/image/Шрерадугас.jpg" width="100" height="100">
-        <figcaption>
-            Мысли все еще свободны?
-        </figcaption>
-        <time datetime="2021-02-03">Опубликовано 03.02.2021</time>
-        <a>goldfels</a>
-    </figure>
-</aside>
-<section>
-    Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла Бла
-</section>
-<header>
-    <h2>Комментарий:</h2>
-</header>
-<aside>
-    <figure>
-        <img src="style/image/бобр.jpeg" width="100" height="100">
-        <figcaption>
-            Мысли все еще свободны?
-        </figcaption>
-        <time datetime="2021-02-04">Опубликовано 03.02.2021</time>
-        <a>Screck</a>
-    </figure>
-    <section>
-        Здравстуйте goldfels, хочу заметить, что ваш пост полное гавно!
-    </section>
-</aside>
-<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
--->
 </body>
 </html>
